d9b8ac312f5549ddb3c7af54d3e8d3a591180f31
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-01-31 11:56:51 -0800

    added router

---
+++ router/notes.router.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+router.get('/', (req, res, next) => {
+  // const searchNotes = val => (val.title.includes(searchTerm) || val.content.includes(searchTerm));
+  let { searchTerm } = req.query;
+  // return (!searchTerm) ? res.json(data) : res.json(data.filter(searchNotes)); 
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(list);
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = parseInt(req.params.id, 10);
+  // res.json(data.find(item => item.id === id));
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      res.json('not found');
+    }
+  });
+});
+
+
+
+module.exports = router; 

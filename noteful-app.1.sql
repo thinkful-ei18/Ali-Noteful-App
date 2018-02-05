4fb84b5f6dcb229a2cfed3b590693a44efd493f1
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-02-05 13:58:55 -0800

    Second push SQL

--- noteful-app.1.sql
+++ noteful-app.1.sql
@@ -2,18 +2,20 @@
 DROP TABLE IF EXISTS notes;
 
 CREATE TABLE notes(
-  id serial PRIMARY KEY AUTO_INCREMENT = 1000,
+  id serial PRIMARY KEY,
   title text NOT NULL,
   content text NOT NULL,
   created timestamp DEFAULT current_timestamp
 );
 
--- --  INSERT INTO notes
--- --    (title, content) VALUES
--- --    ('TEST NOTE 2', 'CANADA'),
--- --    ('TEST NOTE 3', 'GERMANY'),
--- --    ('TEST NOTE 4', 'CHINA'),
--- --    ('TEST NOTE 5', 'INDIA'),
--- --    ('TEST NOTE 6', 'BRAZIL');   
+ALTER SEQUENCE notes_id_seq RESTART WITH 1000;
 
--- SELECT * FROM notes;
+ INSERT INTO notes
+   (title, content) VALUES
+   ('TEST NOTE 2', 'CANADA'),
+   ('TEST NOTE 3', 'GERMANY'),
+   ('TEST NOTE 4', 'CHINA'),
+   ('TEST NOTE 5', 'INDIA'),
+   ('TEST NOTE 6', 'BRAZIL');   
+
+SELECT * FROM notes;
